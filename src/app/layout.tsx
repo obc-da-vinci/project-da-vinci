f7f92715846f6ab60333e1b08b0af4717c1e57fd
Thiago Magno <>
2024-03-19 10:35:01 -0300

    initial config

---
+++ src/app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from 'next'
+import { Montserrat } from 'next/font/google'
+import './globals.css'
+
+const montserrat = Montserrat({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'OBC - Da Vinci',
+  description: 'Plataforma de Agendamento de Serviços',
+}
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode
+}>) {
+  return (
+    <html lang="en">
+      <body className={montserrat.className}>{children}</body>
+    </html>
+  )
+}
