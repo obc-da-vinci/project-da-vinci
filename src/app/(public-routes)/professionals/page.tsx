a90bcdafe975d55e38ef81d619db1fcb09a6e2e9
Thiago Magno <>
2024-03-21 15:26:38 -0300

    update pages: professionals and solutions

--- src/app/(public-routes)/professionals/page.tsx
+++ src/app/(public-routes)/professionals/page.tsx
@@ -1,12 +1,9 @@
-import { getProfessionals } from '@/services/client'
+// import { getProfessionals } from '@/services/client'
 
 export default async function ProfessionalsPage() {
-  const professionals = await getProfessionals()
-
   return (
     <div>
       <h1>Professionals Page</h1>
-      {professionals && <pre>{JSON.stringify(professionals, null, 2)}</pre>}
     </div>
   )
 }
