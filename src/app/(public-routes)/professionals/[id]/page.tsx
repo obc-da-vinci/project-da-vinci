e88280e552dfeeaebdb38e034f8d59685f47b387
Thiago Magno <164017874+obc-da-vinci@users.noreply.github.com>
2024-03-26 16:06:04 -0300

    update test

--- src/app/(public-routes)/professionals/[id]/page.tsx
+++ src/app/(public-routes)/professionals/[id]/page.tsx
@@ -1,18 +1,14 @@
 import PageTitle from '@/components/page-title'
-import { getProfessionalById } from '@/services/client'
 
 export default async function ProfessionalAvailabilityPage({
   params,
 }: {
   params: { id: string }
 }) {
-  const { professional } = await getProfessionalById(params.id)
-
   return (
     <div>
       <PageTitle title="Schedule an appointment" />
       <h1>{params.id}</h1>
-      {professional && <p>{professional.name}</p>}
     </div>
   )
 }
--- src/app/api/professionals/route.ts
+++ src/app/api/professionals/route.ts
@@ -1,43 +1,19 @@
 import { prisma } from '@/lib/prisma'
 
 // GET /professionals - Obter lista de profissionais.
-export async function GET(request: Request) {
-  const { searchParams } = new URL(request.url)
-  const id = searchParams.get('id')
-
+export async function GET() {
   try {
-    if (id) {
-      const data = await prisma.professional.findUnique({
-        where: { id },
-      })
+    const data = await prisma.professional.findMany({
+      orderBy: { name: 'asc' },
+    })
 
-      if (!data) {
-        return Response.json(
-          { error: 'Professional not found' },
-          { status: 404 },
-        )
-      }
+    const professionals = data.map((item) => ({
+      id: item.id,
+      name: item.name,
+      email: item.email,
+    }))
 
-      const professional = {
-        id: data.id,
-        name: data.name,
-        email: data.email,
-      }
-
-      return Response.json({ professional })
-    } else {
-      const data = await prisma.professional.findMany({
-        orderBy: { name: 'asc' },
-      })
-
-      const professionals = data.map((item) => ({
-        id: item.id,
-        name: item.name,
-        email: item.email,
-      }))
-
-      return Response.json({ professionals })
-    }
+    return Response.json({ professionals })
   } catch (e) {
     console.error('Error fetching professionals:', e)
     return Response.json(
